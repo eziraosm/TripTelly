0274b4f2570e4485137ce8f28c7d8628d1d70ce9
Usamah Thani <usamah@naditeknologi.com.my>
2024-11-29 12:30:16 +0800

    update: now database is online

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,21 @@
 <?php
-    $hostname = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "triptelly";
+    // $hostname = "localhost";
+    // $user = "root";
+    // $password = "";
+    // $database = "triptelly";
+
+    // $conn = mysqli_connect($hostname, $user, $password, $database) OR DIE ("Connection failed!");
+    
+    // if (!$conn) {
+    //     die("Connection failed: " . mysqli_connect_error());
+    // }
+?>
+
+<?php
+    $hostname = "usamahthani.com";
+    $user = "usamahthani_host";
+    $password = ";bp0oFyD&vr3";
+    $database = "usamahthani_triptelly";
 
     $conn = mysqli_connect($hostname, $user, $password, $database) OR DIE ("Connection failed!");
     
