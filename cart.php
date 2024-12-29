f63911d4d12e63527a0c47c3ff6267c03395c5e9
Sam PC <mohamadusamahthani@gmail.com>
2024-12-29 15:09:01 +0800

    fix: dropdown in navbar in cart now working

--- cart.php
+++ cart.php
@@ -127,6 +127,12 @@ if (!isset($_SESSION['form_data_cart'])) {
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
         crossorigin="anonymous"></script>
 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap Bundle (includes Popper.js for dropdowns) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 
     <link rel="stylesheet" href="assets/css/cart.css">
 </head>
@@ -265,11 +271,11 @@ if (!isset($_SESSION['form_data_cart'])) {
                                                 <hr>
                                             <?php }
                                             $disable = "";
-                                            if (!isset($form_data))  {
+                                            if (!isset($form_data)) {
                                                 $disable = "disabled";
-                                            } 
-                                            if (!$hasHotelItems) {  
-                                            ?>
+                                            }
+                                            if (!$hasHotelItems) {
+                                                ?>
                                                 <div class="row">
                                                     <div class="col-12 text-center">
                                                         <a href="searchHotel.php" class="btn btn-primary" <?= $disable ?>>Search
